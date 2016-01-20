116b72991822aacda3cc9a2c762f4d74b27e4cc5
maddyonline <madhavjha@gmail.com>
2016-01-20 02:19:19 -0800

    More

--- tags/hello-world.js
+++ tags/hello-world.js
@@ -1,2 +1,2 @@
-riot.tag2('hello-world', '<h2>Hello world</h2>', '', '', function(opts) {
-});
+riot.tag2('hello-world', '<h2>Hello {opts.greet}</h2>', '', '', function(opts) {
+}, '{ }');
--- tags/hello-world.tag
+++ tags/hello-world.tag
@@ -1,3 +1,3 @@
 <hello-world>
-  <h2>Hello world</h2>
+  <h2>Hello {opts.greet}</h2>
 </hello-world>
